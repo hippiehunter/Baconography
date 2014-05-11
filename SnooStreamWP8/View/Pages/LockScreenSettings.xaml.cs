52ec350e9b27adb78b00571fd44cdb5b45cca73d
MLewis <marty.lewis@synergex.com>
2014-05-10 17:47:18 -0700

    More WIP UI

---
+++ SnooStreamWP8/View/Pages/LockScreenSettings.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SnooStreamWP8.Common;
+
+namespace SnooStreamWP8.View.Pages
+{
+	public partial class LockScreenSettings : SnooApplicationPage
+	{
+		public LockScreenSettings()
+		{
+			InitializeComponent();
+		}
+	}
+}
