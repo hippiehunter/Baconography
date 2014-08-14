d2f7530bd552942328d36b8fa58bb2b25b00cd37
Jeff Greene <hippiehunterenator@gmail.com>
2014-08-14 14:44:34 -0700

    added missing file and fixed ignore

---
+++ SnooStream/SnooStream.Shared/Common/MarkdownHelpers.cs
@@ -0,0 +1,35 @@
+﻿using SnooDom;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnooStream.Common
+{
+    class MarkdownHelpers : IStyleProvider, ICommandFactory
+    {
+		public Windows.Foundation.TypedEventHandler<Windows.UI.Xaml.Documents.Hyperlink, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs> MakeLinkCommand(string url)
+		{
+			return new Windows.Foundation.TypedEventHandler<Windows.UI.Xaml.Documents.Hyperlink, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs>((link, bla) => { });
+		}
+
+		public Windows.UI.Xaml.Style BorderStyle
+		{
+			get { return null; }
+		}
+
+		public Windows.UI.Xaml.Style RichTextBlockStyle
+		{
+			get { return null; }
+		}
+
+		public Windows.UI.Xaml.Style RunStyle
+		{
+			get { return null; }
+		}
+
+		public Windows.UI.Xaml.Style TextBlockStyle
+		{
+			get { return null; }
+		}
+	}
+}
